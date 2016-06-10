1a24c5c9b03a830244986a2ee6715ea85f5144af
Aleksandar Panic <Aleksandar Panic>
2016-06-10 08:54:53 +0200

    Added app manifest to request admin privileges and use dpi awareness.

--- VagrantBackup/Program.cs
+++ VagrantBackup/Program.cs
@@ -8,16 +8,12 @@ namespace VagrantBackup
 {
     static class Program
     {
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        private static extern bool SetProcessDPIAware();
-
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            if (Environment.OSVersion.Version.Major >= 6) SetProcessDPIAware();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
